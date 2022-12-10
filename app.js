ca0ec35353d40d5c4aea263710989b83ed7c433d
Dobernadze <tima7723@gmail.com>
2022-12-10 17:20:45 +0600

    second commit

--- app.js
+++ app.js
@@ -3,24 +3,44 @@ const fs = require("fs");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+    
     if(req.url === "/"){
         res.writeHead(200, {"Content-Type": "text/html"});
-        fs.readFile("page/home.html", (err,data) => {
+        fs.readFile('page/home.html','utf8', (err,data) => {
             if (err) throw err;
             res.write(data)
+            res.end()
         })
     }
-    if(req.url === "/about"){
+   
+   else if(req.url === "/about"){
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('page/about.html','utf8', (err,data) => {
+        if (err) throw err;
+        res.write(data)
+        res.end()
+    });
+} 
+   
+else if(req.url === "/create-file"){
+ res.writeHead(200, {"Content-Type": "text/html"});
+ fs.writeFile('temp/test.html','<h1>This is a test file</h1>', (err) => {
+     if (err) throw err;
+     res.write('File is created')
+     res.end()
+ });
+} 
+        else {
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("<h1>About Us<h1>")
-        
+        fs.readFile('page/error.html','utf8', (err,data) => {
+            if (err) throw err;
+            res.write(data)
+            res.end()  
+        });
+    } 
+})
       
-        } else {
-            res.writeHead(404, {"Content-Type": "text/html"});
-        res.write("<h1>Page not found<h1>")
         
-        }
-        res.end()   
-}); 
-
 server.listen(3000);
+        
+        
